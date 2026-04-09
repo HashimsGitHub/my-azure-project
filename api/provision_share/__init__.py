89a5ee0ce21f95cce9f6571a4c6dfa68fd4afb61
HashimHilal-QUT <hashim.hilal@connect.qut.edu.au>
2026-04-09 14:52:40 +1000

    Now add MongoDB:

--- api/provision_share/__init__.py
+++ api/provision_share/__init__.py
@@ -2,6 +2,7 @@ import azure.functions as func
 import os
 from azure.storage.fileshare import ShareServiceClient
 from azure.core.exceptions import ResourceExistsError
+from pymongo import MongoClient
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("storage imports OK", status_code=200)
+    return func.HttpResponse("all imports OK", status_code=200)
