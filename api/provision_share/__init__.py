6eef015134d1429753523d5b4baa5948afb352a4
HashimHilal-QUT <hashim.hilal@connect.qut.edu.au>
2026-04-09 14:49:21 +1000

    The function registration is clean now. Add the imports back one at a time to find exactly where it breaks.

--- api/provision_share/__init__.py
+++ api/provision_share/__init__.py
@@ -1,5 +1,7 @@
 import azure.functions as func
 import os
+from azure.storage.fileshare import ShareServiceClient
+from azure.core.exceptions import ResourceExistsError
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("provision_share alive", status_code=200)
+    return func.HttpResponse("storage imports OK", status_code=200)
